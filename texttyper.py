47c100aa7d07a503ad538d70bc19f75319557b3b
Lonofera <lonofera0@gmail.com>
2024-01-11 13:25:03 +0300

    Update texttyper.py

--- texttyper.py
+++ texttyper.py
@@ -27,17 +27,6 @@ time.sleep(1)
 typingPrint("You've entered the Matrix!\n")
 time.sleep(1)
 
-pillColor = typingInput("Will you take the blue or the red pill? (Type b for blue, r for red)")
-
-if pillColor == "b":
-    typingPrint("You took the blue pill! ")
-    typingPrint("You are leaving the Matrix and going back to the real world!\n")
-elif pillColor == "r":
-    typingPrint("You took the red pill! ")
-    typingPrint("You will be stuck in the Matrix forever!\n")
-else:
-    typingPrint("Invalid answer!")
-
 time.sleep(1)
 typingPrint("Good bye!\n")
 typingPrint("This screen will clear itself in 3..")
